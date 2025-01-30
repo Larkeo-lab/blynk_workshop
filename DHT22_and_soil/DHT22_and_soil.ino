e91db2cd06d368a718cc5ce4c18e68b6254ea042
Dee s1 <141470070+Larkeo-lab@users.noreply.github.com>
2025-01-30 17:17:00 +0700

    Update DHT22_and_soil.ino

--- DHT22_and_soil/DHT22_and_soil.ino
+++ DHT22_and_soil/DHT22_and_soil.ino
@@ -4,7 +4,6 @@
 #define BLYNK_TEMPLATE_NAME "DHT22"
 #define BLYNK_AUTH_TOKEN "1zqvVmKeDWXCKNNqzfplgHEYgu11lb0M"
 
-
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <DHT.h>
@@ -15,8 +14,8 @@
 DHT dht(DHTPIN, DHTTYPE);
 int SOIL_PIN = A0;
 
-char ssid[] = "Dee";
-char pass[] = "bbbbbbbb";
+char ssid[] = "YOUR_WIFI_NAME";
+char pass[] = "YOUR_PASSOWRD";
 
 void setup()
 {
@@ -25,7 +24,6 @@ void setup()
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
   pinMode(SOIL_PIN, INPUT);
 }
-
 
 void dht22Data() {
     float h = dht.readHumidity();
@@ -37,20 +35,17 @@ void dht22Data() {
     Serial.print("Humidity: "); Serial.print(h);
     Serial.print(" %\t Temperature: "); Serial.print(t);
     Serial.println(" °C");
-
-    Blynk.virtualWrite(V1, t);
-    Blynk.virtualWrite(V2, h);
+    Blynk.virtualWrite(V0, t);
+    Blynk.virtualWrite(V1, h);
 }
 void soilData()
 {
     int soilValue = analogRead(SOIL_PIN); 
     int moisturePercent = map(soilValue, 1023, 0, 0, 100);
-
     Serial.print("Soil Moisture: ");
     Serial.print(moisturePercent);
     Serial.println("%");
-
-    Blynk.virtualWrite(V3, moisturePercent);
+    Blynk.virtualWrite(V2, moisturePercent);
 }
 
 void loop()
